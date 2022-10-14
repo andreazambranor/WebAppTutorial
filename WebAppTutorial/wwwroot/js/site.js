f31f26a0090cc764ede25224daf705c14eebfd86
andreazambranor <azambranor.23@gmail.com>
2022-10-14 17:55:46 -0400

    Add project files.

---
+++ WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// FUNCTIONS TO BE CALLED ON MOUSEOVER AND ONMOUSEOUT TO CHANGE COLOR OF HELLO WORLD
+function newColor() {
+    document.getElementById("hello_world").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
